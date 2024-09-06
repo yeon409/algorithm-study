ca9880cfc43f2c44148adb3648e66c552ad20da5
Soyeon You <71324520+yeon409@users.noreply.github.com>
2024-09-06 11:39:08 +0900

    Time: 703 ms (86.99%), Space: 54.6 MB (81.13%) - LeetHub

--- cindy/3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.py
+++ cindy/3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.py
@@ -5,14 +5,37 @@
 #         self.next = next
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        dic = {i:1 for i in nums}
-        ans = ListNode()
-        cur = ans
+        # dic = {i:1 for i in nums}
+        # ans = ListNode()
+        # cur = ans
 
-        while head != None:
-            if head.val not in dic.keys():
-                cur.next = ListNode(head.val)
-                cur = cur.next
+        # while head != None:
+        #     if head.val not in dic.keys():
+        #         cur.next = ListNode(head.val)
+        #         cur = cur.next
+        #     head = head.next
+
+        # return ans.next
+
+        s = set(nums)
+
+        while head and head.val in s:
             head = head.next
+        
+        if not head:
+            return None
+        
+        prev = head
+        curr = head.next
 
-        return ans.next
+        while curr:
+            if curr.val not in s:
+                prev.next = curr
+                prev = curr
+            curr = curr.next
+        
+        prev.next = None
+        
+        return head
+        
+        
